4427bd7dfbf5232f218b7e5f0511271bbcb4917d
Mateusz Iwan <143963948+shieq11@users.noreply.github.com>
2023-09-25 09:33:17 +0200

    poprawka views

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for mysite project.
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,17 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from blog.views import Image, ImageDisplay
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('blog.urls')),
-    path('image/', Image.as_view(), name='image'),
-    path('image/<int:pk>/', ImageDisplay.as_view(), name="image_display")
+    path('', include('blog.urls')),
+
 ]
 handler404 = 'blog.views.error_404_view'
 
 if settings.DEBUG:
-    urlpatterns +=static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

60b43c1db370e6ca70726a178b93895cbabf2f9a
Mateusz Iwan <143963948+shieq11@users.noreply.github.com>
2023-09-26 11:44:28 +0200

    poprawka views

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
 
-# Create your tests here.
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get(url='http://127.0.0.1:8000/admin')
+
+def element_is_clickable():
+    driver.find_element(By.XPATH, '//*[@id="id_username"]' ).send_keys("shieq") # wpisywanie loginu
+    driver.find_element(By.XPATH, '//*[@id="id_password"]').send_keys("Brzezinki77B") #wpisywanie hasła
+    time.sleep(2)
+    driver.find_element(By.XPATH, '//*[@id="login-form"]/div[3]/input').click() # klikniecie
+    time.sleep(5)
+    driver.find_element(By.XPATH, '//*[@id="user-tools"]/a[1]').click()
+    driver.find_element(By.XPATH, '/html/body/nav/div/a/span').click()
+
+#element_is_clickable()
+
+def response_check(w,file):
+    height = 768
+    driver.set_window_size(w,height)
+    driver.save_screenshot(file)
+
+response_check(900, "test900.png")
+response_check(1200, "test1200.png")
+response_check(1800, "test1800.png")
+response_check(600, "test600.png")
